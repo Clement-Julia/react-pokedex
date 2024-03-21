b41d0e375c331cd6601ee057102823873e59a859
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2024-03-21 16:41:57 +0100

    feat(home): add more fetch poke

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
     const [pokemons, setPokemons] = useState([]);
+    const [offset, setOffset] = useState(152);
+    const [limit, setLimit] = useState(50);
     let test = searchParams.get("query");
     
     const [loading, setLoading] = useState(true);
@@ -18,7 +20,7 @@ const Home = () => {
             .get('https://pokeapi.co/api/v2/pokemon', {
                 params: {
                     offset: 0,
-                    limit: 151
+                    limit: test ? 1500 : 151
                 }
             })
             .then((response) => {
@@ -38,7 +40,6 @@ const Home = () => {
                         return filteredPokemons;
                     }, []);
 
-                    console.log(pokemons);
                     setPokemons(pokemons);
                     setLoading(false);
                 });
@@ -49,8 +50,76 @@ const Home = () => {
             });
     }, []);
 
+    const fetchMorePokemons = () => {
+        axios
+        .get('https://pokeapi.co/api/v2/pokemon', {
+            params: {
+                offset: offset,
+                limit: limit
+            }
+        })
+        .then((response) => {
+            const promises = response.data.results.map(pokemon => {
+                return axios.get(pokemon.url);
+            });
+
+            Promise.all(promises).then(pokemonResponses => {
+                const newPokemons = pokemonResponses.reduce((filteredPokemons, pokemonResponse) => {
+                    if(!test || pokemonResponse.data.name.toLocaleLowerCase().includes(test)){
+                        filteredPokemons.push({
+                            id: pokemonResponse.data.id,
+                            name: pokemonResponse.data.name.charAt(0).toUpperCase() + pokemonResponse.data.name.slice(1),
+                            type: pokemonResponse.data.types.map(type => type.type.name)
+                        });
+                    }
+                    return filteredPokemons;
+                }, []);
+
+                setPokemons(prevPokemons => [...prevPokemons, ...newPokemons]);
+                setOffset(offset + 50);
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
+    const fetchAllPokemons = async () => {
+        axios
+        .get('https://pokeapi.co/api/v2/pokemon', {
+            params: {
+                offset: offset,
+                limit: 1510
+            }
+        })
+        .then((response) => {
+            const promises = response.data.results.map(pokemon => {
+                return axios.get(pokemon.url);
+            });
+
+            Promise.all(promises).then(pokemonResponses => {
+                const newPokemons = pokemonResponses.reduce((filteredPokemons, pokemonResponse) => {
+                    if(!test || pokemonResponse.data.name.toLocaleLowerCase().includes(test)){
+                        filteredPokemons.push({
+                            id: pokemonResponse.data.id,
+                            name: pokemonResponse.data.name.charAt(0).toUpperCase() + pokemonResponse.data.name.slice(1),
+                            type: pokemonResponse.data.types.map(type => type.type.name)
+                        });
+                    }
+                    return filteredPokemons;
+                }, []);
+
+                setPokemons(prevPokemons => [...prevPokemons, ...newPokemons]);
+                setLimit(0);
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
     return (
-        <div className='px-1 py-1 mt-5'>
+        <div className='px-1 py-1 mt-3'>
             <h1 className='title d-flex justify-content-center mb-4'>Liste des pokémons</h1>
             <div className='row w-100'>
                 {loading ? (
@@ -58,9 +127,18 @@ const Home = () => {
                         <span className="loader"></span>
                     </div>
                 ) : pokemons.length > 0 ? (
-                    pokemons.map((pokemon) => {
-                        return <Card key={pokemon.id} pokemon={pokemon}></Card>
-                    })
+                    <>
+                        {pokemons.map((pokemon) => {
+                            return <Card key={pokemon.id} pokemon={pokemon}></Card>
+                        })}
+                        { (limit > 0) && !test ? (
+                            <div className="d-flex justify-content-center my-2">
+                                <button className="btn btn-primary me-2" onClick={fetchMorePokemons}>Show 50</button>
+                                <button className="btn btn-primary" onClick={fetchAllPokemons}>Show all</button>
+                            </div>
+                        ) : null
+                    }
+                    </>
                 ) : (
                     <p>No pokemons found</p>
                 )}
--- src/components/routes/Routeur.js
+++ src/components/routes/Routeur.js
@@ -4,6 +4,7 @@ import Navigation from '../layout/navigation'
 
 import NotFound from './error-page'
 import Home from '../pages/Home'
+import ViewPokemon from '../pages/ViewPokemon'
 
 const Routeur = () => {
 	return (
@@ -11,6 +12,7 @@ const Routeur = () => {
 			<Navigation />
 			<Routes>
 				<Route exact path='/' element={<Home />} />
+				<Route exact path='pokemon/:id' element={<ViewPokemon />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
