7431c3b566721704a0e3a1c37e5882d647a5aa83
raph <raphaeltisba@yahoo.fr>
2024-03-22 08:54:26 +0100

    ajout: update card.js

--- src/components/layout/card.js
+++ src/components/layout/card.js
@@ -1,6 +1,28 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
-const Card = ({ pokemon }) => {
+const Card = ({ pokemon, updateFav }) => {
+	const [isHovered, setIsHovered] = useState(false)
+	const [isFavorite, setIsFavorite] = useState(false)
+
+	const mouseEnter = () => {
+		setIsHovered(true)
+	}
+	const mouseLeave = () => {
+		setIsHovered(false)
+	}
+	const handleUpdateFavorite = () => {
+		setIsFavorite(!isFavorite)
+		updateFav(pokemon.id)
+		window.location.reload()
+	}
+
+	// Gère l'affichage de l'étoile selon présence dans le pokédex
+	useEffect(() => {
+		const pokedex = JSON.parse(localStorage.getItem('pokedex')) || []
+		setIsFavorite(pokedex.includes(pokemon.id))
+	}, [pokemon.id])
+
 	let id = pokemon.id
 	let typeClass = ''
 
@@ -11,23 +33,34 @@ const Card = ({ pokemon }) => {
 		id = '00' + id
 	}
 
-	pokemon.type.map((type, i) => {
-		typeClass += ` type-${type.toLowerCase()}`
-	})
+	{
+		pokemon.types.map((type, i) => {
+			typeClass += ` type-${type.toLowerCase()}`
+		})
+	}
 
 	return (
 		<div className='col-3 my-3'>
-			<Link className='text-decoration-none' key={pokemon.id} to={{ pathname: `/pokemon/${pokemon.id}` }}>
+			{/* <Link className='text-decoration-none' key={pokemon.id} to={{ pathname: `/pokemon/${pokemon.id}` }}> */}
 				<div className={`card m-1 row flex-row ${typeClass}`}>
 					<div className='bg-pokeball'></div>
 					<span className='pokemon-id'>{`#${id}`}</span>
-					<h2>{pokemon.name}</h2>
+					<div className='d-flex justify-content-between align-items-center'>
+						<h2>{pokemon.name}</h2>
+						<div onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+							{isFavorite || isHovered ? (
+								<i className="fa-solid fa-star fav" onClick={handleUpdateFavorite}></i>
+							) : (
+								<i className="fa-regular fa-star fav" onClick={handleUpdateFavorite}></i>
+							)}
+						</div>
+					</div>
 					<div className='card-title col-6'>
 						<div className='pokemon-types mt-1'>
-							{pokemon.type.map((type, i) => {
+							{pokemon.types.map((type, i) => {
 								return (
 									<span key={i} className='type'>
-										{type.charAt(0).toUpperCase() + type.slice(1)}
+										{type}
 									</span>
 								)
 							})}
@@ -39,7 +72,7 @@ const Card = ({ pokemon }) => {
 						{/* <img alt="bulbasaur" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"/> */}
 					</div>
 				</div>
-			</Link>
+			{/* </Link> */}
 		</div>
 	)
 }
