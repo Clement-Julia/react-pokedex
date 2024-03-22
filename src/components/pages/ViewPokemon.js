0852d5c427e7f1aac37831bbf964f7a32eb8ffba
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2024-03-22 10:12:00 +0100

    fix(home): fix fectch poke button

--- src/components/pages/ViewPokemon.js
+++ src/components/pages/ViewPokemon.js
@@ -38,7 +38,7 @@ const ViewPokemon = () => {
     }
     
     var idPokemon = id
-    if (id > 10 & id < 100) {
+    if (id >= 10 & id < 100) {
         idPokemon = '0' + idPokemon
     }
     if (id < 10) {
